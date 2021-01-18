0639594c972b2fbabb40f5a3b3b79abc6cf05912
= <=>
2021-01-18 16:34:41 -0500

    wrote soome new routes. Grid is not looking as I liked. Will have to measure performance

--- src/App.js
+++ src/App.js
@@ -5,17 +5,31 @@ import './css/materialize.css'
 
 import Navbar from './components/Nav/Navbar.js'
 import Home from './components/Home.js'
+import About from './components/About.js'
+
+import { BrowserRouter as Router,
+        Route, 
+        Switch
+} from 'react-router-dom'
 
 
 class App extends React.Component{
     render() {
         return (
+            <Router>
             <div id = 'App'>
                 <Navbar />
-                <Home />
-                
+                <Switch>
+                    <Route path = "/about">
+                        <About />
+                    </Route>
+                    <Route path = "/">
+                        <Home />
+                    </Route>
+                </Switch> 
             </div>
-        )
+            </Router>
+        );
     }
 }
 
